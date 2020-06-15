8640464648686ab512cff5bf76baf8b3976ca561
hbs <hbsowo58@gmail.com>
2020-06-15 11:23:23 +0900

    feat: add v7 axios ver

---
+++ v7/public/todosv7.js
@@ -0,0 +1,138 @@
+let todos = [];
+
+const $todos = document.querySelector(".todos");
+const $inputTodo = document.querySelector(".input-todo");
+const $ckAll = document.querySelector("#ck-complete-all");
+const $completedTodos = document.querySelector(".completed-todos");
+const $activeTodos = document.querySelector(".active-todos");
+const $clearCompleted = document.querySelector(".clear-completed > .btn");
+const $nav = document.querySelector(".nav");
+let navState = "all";
+
+const getTodos = () => {
+  axios
+    .get("todos/")
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+
+const render = () => {
+  const _todos = todos.filter((todo) => {
+    if (navState === "active") return !todo.completed;
+    if (navState === "completed") return todo.completed;
+    return true;
+  });
+  let html = "";
+
+  _todos.forEach(({ id, content, completed }) => {
+    html += `<li id="${id}" class="todo-item">
+    <input class="custom-checkbox" type="checkbox" id="ck-${id}" ${
+      completed ? "checked" : ""
+    }>
+    <label for="ck-${id}">${content}</label>
+    <i class="remove-todo far fa-times-circle"></i>
+  </li>`;
+  });
+
+  $completedTodos.textContent = todos.filter((todo) => todo.completed).length;
+  $activeTodos.textContent = todos.filter((todo) => !todo.completed).length;
+  $ckAll.checked = todos.length
+    ? todos.filter((todo) => todo.completed).length === todos.length
+    : false;
+  $todos.innerHTML = html;
+};
+
+const generateId = () => {
+  return todos.length ? Math.max(...todos.map((todo) => todo.id)) + 1 : 1;
+};
+
+const addTodo = (content) => {
+  const payload = { id: generateId(), content, completed: false };
+  axios
+    .post("todos/", payload)
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+
+const removeTodo = (id) => {
+  axios
+    .delete(`todos/${id}`)
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+
+const toggleTodo = (id) => {
+  axios
+    .patch(`todos/${id}`, { completed })
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+
+const completeAll = (completed) => {
+  axios
+    .patch("todos/", { completed })
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+const clearCompleted = () => {
+  axios
+    .delete("todos/completed")
+    .then((response) => {
+      todos = response.data;
+    })
+    .catch((error) => console.log(error))
+    .then(render);
+};
+
+$inputTodo.onkeyup = (e) => {
+  const content = $inputTodo.value.trim();
+  if (content === "" || e.keyCode !== 13) return;
+  $inputTodo.value = "";
+  addTodo(content);
+};
+
+$todos.onclick = (e) => {
+  if (!e.target.matches(".remove-todo")) return;
+  removeTodo(e.target.parentNode.id);
+};
+
+$todos.onchange = (e) => {
+  toggleTodo(e.target.parentNode.id);
+};
+
+$ckAll.onchange = () => {
+  completeAll($ckAll.checked);
+};
+
+$clearCompleted.onclick = () => {
+  clearCompleted();
+};
+
+$nav.addEventListener("click", ({ target }) => {
+  if (!target.matches(".nav > li")) return;
+  const $active = $nav.querySelector(".active");
+
+  if ($active === target) return;
+  $active.classList.remove("active");
+  target.classList.add("active");
+
+  navState = target.id;
+  render();
+});
+
+window.onload = getTodos;
